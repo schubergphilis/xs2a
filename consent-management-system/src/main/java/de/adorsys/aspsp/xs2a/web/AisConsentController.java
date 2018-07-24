2c3baa9c8b3f4781ea3129d314f386c983236dc9
Petro Rudenko <pru@adorsys.com.ua>
2018-07-24 14:44:55 +0300

    migrated to liquibase xml file instead of sql

--- consent-management-system/src/main/java/de/adorsys/aspsp/xs2a/web/AisConsentController.java
+++ consent-management-system/src/main/java/de/adorsys/aspsp/xs2a/web/AisConsentController.java
@@ -86,7 +86,7 @@ public class AisConsentController {
     public ResponseEntity<Void> updateConsentStatus(
         @ApiParam(name = "consent-id", value = "The account consent identification assigned to the created account consent.", example = "bf489af6-a2cb-4b75-b71d-d66d58b934d7")
         @PathVariable("consent-id") String consentId,
-        @ApiParam(value = "The following code values are permitted 'valid', 'rejected', 'revoked by psu', 'terminated by tpp'. These values might be extended by ASPSP by more values.", example = "VALID")
+        @ApiParam(value = "The following code values are permitted 'VALID', 'REJECTED', 'REVOKED_BY_PSU', 'TERMINATED_BY_TPP'. These values might be extended by ASPSP by more values.", example = "VALID")
         @PathVariable("status") String status) {
         return aisConsentService.updateConsentStatusById(consentId, SpiConsentStatus.valueOf(status))
                    .map(updated -> new ResponseEntity<Void>(HttpStatus.OK))
