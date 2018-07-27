9fa649ab4aca255457e489456724d8e5be7b6b78
Christina Chomatas <cch@adorsys.de>
2018-07-27 10:48:25 +0200

    WIP: Adapt Feature file, add expected result, refactor

--- integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/PISStatusSteps.java
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/PISStatusSteps.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en_scouse.An;
 import de.adorsys.aspsp.xs2a.domain.pis.SinglePayments;
 import de.adorsys.aspsp.xs2a.integtest.model.TestData;
 import de.adorsys.aspsp.xs2a.integtest.util.Context;
@@ -36,11 +37,21 @@ public class PISStatusSteps {
         @Given("^PSU is logged in$")
     */
 
-    @And("^created a payment status request with of a not existing payment-id (.*) using the payment product (.*)$")
+    @And("^created a payment status request without an existing payment-id (.*) using the payment product (.*)$")
     public void setPaymentParameters(String paymentId, String paymentProduct) throws IOException {
         context.setPaymentProduct(paymentProduct);
         context.setPaymentId(paymentId);
+    }
 
+    @And("^and the set of data (.*)$")
+    public void loadTestData(String dataFileName) throws IOException {
+        File jsonFile = new File("src/test/resources/data-input/pis/status/" + dataFileName);
+
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        TestData data = mapper.readValue(jsonFile, new TypeReference<TestData>() {
+        });
+
+        context.setTestData(data);
     }
 
     @When("^PSU requests the status of the payment$")
@@ -50,9 +61,9 @@ public class PISStatusSteps {
         headers.add("x-request-id", "2f77a125-aa7a-45c0-b414-cea25a116035");
         headers.add("Authorization", "Bearer " + context.getAccessToken());
 
-        HttpEntity<SinglePayments> entity = new HttpEntity (headers);
+        HttpEntity entity = new HttpEntity(headers);
 
-        // TO DO: clarify return type, actual String
+        // TODO: clarify return type, actual String
         ResponseEntity<HashMap> response = restTemplate.exchange(
             context.getBaseUrl() + "/payments/" + context.getPaymentProduct() + "/" + context.getPaymentId() + "/status",
             HttpMethod.GET,
@@ -62,15 +73,17 @@ public class PISStatusSteps {
         context.setResponse(response);
     }
 
-    @Then("^an appropriate response code and the status (.*) is delivered to the PSU$")
-    public void checkStatus(String dataFileName) {
+    @Then("^an appropriate response code and the status is delivered to the PSU$")
+    public void checkStatus() {
 
-        File jsonFile = new File("src/test/resources/data-input/pis/status/" + dataFileName);
-
-
+        ResponseEntity<HashMap> actualResponse = context.getResponse();
+        HashMap<String, String> givenResponseBody = (HashMap) context.getTestData().getResponse().getBody();
+        HttpStatus compareStatus = convertStringToHttpStatusCode(context.getTestData().getResponse().getCode());
+        assertThat(actualResponse.getStatusCode(), equalTo(compareStatus));
+        assertThat(actualResponse.getBody().get("transactionStatus"), equalTo(givenResponseBody.get("transactionStatus")));
     }
 
-    private HttpStatus convertStringToHttpStatusCode(String code){
+    private HttpStatus convertStringToHttpStatusCode(String code) {
         return HttpStatus.valueOf(Integer.valueOf(code));
     }
 
--- integration-tests/src/test/resources/features/PIS_01.feature
+++ integration-tests/src/test/resources/features/PIS_01.feature
@@ -110,9 +110,10 @@ Feature: Payment Initiation Service
 
     Scenario Outline: Payment Status Request with not existing Payment-ID
         Given PSU is logged in using redirect approach
-        And created a payment status request with of a not existing payment-id <payment-id> using the payment product <payment-product>
+        And created a payment status request without an existing payment-id <payment-id> using the payment product <payment-product>
+        And and the set of data <payment-status>
         When PSU requests the status of the payment
-        Then an appropriate response code and the status <payment-status> is delivered to the PSU
+        Then an appropriate response code and the status is delivered to the PSU
         Examples:
             | payment-id                           | payment-product       | payment-status                     |
             | 529e0507-7539-4a65-9b74-bdf87061e99b | sepa-credit-transfers | paymentStatus-not-existing-id.json |
