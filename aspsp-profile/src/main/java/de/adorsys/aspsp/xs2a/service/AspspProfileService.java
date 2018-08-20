38ed95d951de8d31be424db44d308a8f5de7f56f
Mariia Polikarpova <35561575+maypolikarpova@users.noreply.github.com>
2018-08-20 11:14:42 +0200

    added transaction lifetime option

--- aspsp-profile/src/main/java/de/adorsys/aspsp/xs2a/service/AspspProfileService.java
+++ aspsp-profile/src/main/java/de/adorsys/aspsp/xs2a/service/AspspProfileService.java
@@ -233,4 +233,20 @@ public class AspspProfileService {
     public void updateConsentLifetime(int consentLifetime) {
         profileConfiguration.setConsentLifetime(consentLifetime);
     }
+
+    /**
+     * Read the limit of a maximum lifetime of transaction
+     */
+    public int getTransactionLifetime() {
+        return profileConfiguration.getTransactionLifetime();
+    }
+
+    /**
+     * Update the value of a maximum lifetime of transaction
+     *
+     * @param transactionLifetime the value of a maximum lifetime of consent to substitute existing one
+     */
+    public void updateTransactionLifetime(int transactionLifetime) {
+        profileConfiguration.setTransactionLifetime(transactionLifetime);
+    }
 }
