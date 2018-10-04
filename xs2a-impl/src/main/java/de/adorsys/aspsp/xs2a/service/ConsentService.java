8c438385046b801dc16a38159d3f0b7e2d62eec4
ihorkuchevskiy <iku@adorsys.com.ua>
2018-10-04 12:20:35 +0300

    refactoring

--- xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/service/ConsentService.java
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/service/ConsentService.java
@@ -142,14 +142,15 @@ public class ConsentService { //TODO change format of consentRequest to mandator
     }
 
     ResponseObject<Xs2aAccountAccess> getValidatedConsent(String consentId) {
-        SpiAccountConsent spiAccountConsent = Optional.ofNullable(getValidatedSpiAccountConsent(consentId))
-                                                   .filter(consent -> consent.getValidUntil().isAfter(LocalDate.now()))
-                                                   .orElse(null);
-        AccountConsent consent = aisConsentMapper.mapToAccountConsent(spiAccountConsent);
-        if (consent == null) {
+        SpiAccountConsent spiAccountConsent = getValidatedSpiAccountConsent(consentId);
+
+        if (spiAccountConsent == null || LocalDate.now().compareTo(spiAccountConsent.getValidUntil()) >= 0) {
             return ResponseObject.<Xs2aAccountAccess>builder()
                        .fail(new MessageError(new TppMessageInformation(MessageCategory.ERROR, MessageErrorCode.CONSENT_UNKNOWN_400))).build();
         }
+
+        AccountConsent consent = aisConsentMapper.mapToAccountConsent(spiAccountConsent);
+
         if (!consent.isValidStatus()) {
             return ResponseObject.<Xs2aAccountAccess>builder()
                        .fail(new MessageError(new TppMessageInformation(MessageCategory.ERROR, MessageErrorCode.CONSENT_EXPIRED))).build();
